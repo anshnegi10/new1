586b40bfa38fce70751becc6f1659db3072f75b6
Ansh Negi <ansh.negi1233@gmail.com>
2026-04-18 15:26:56 +0530

    Fixed Dockerfile (removed .txt extension again

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(_name_)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask on Ubuntu Docker"
+    return "Hello from ANSH Negi"
 
 if _name_ == "_main_":
     app.run(host="0.0.0.0", port=8000)
